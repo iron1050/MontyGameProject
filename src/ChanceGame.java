fdb6e2c74969c42db41b4dde83d968e720defd65
Chukwuma Dibia <chukwumadibia@gmail.com>
2023-11-04 00:03:58 -0400

    yes

--- src/ChanceGame.java
+++ src/ChanceGame.java
@@ -15,7 +15,8 @@ public class ChanceGame {
     }
 
     public int doorChoose() {
-        return (int) (Math.random() * 3) + 1;
+
+        return (int) ((Math.random() * 3) + 1);
     }
 
     public boolean isWin(int guess) {
@@ -23,8 +24,112 @@ public class ChanceGame {
             return true;
         }else { return false;}
     }
+    public int doorSwitch() {
+        if (playerChoice == 1 && winningDoor == 1) {
+            int oneTime = (int) (Math.random() * 2);
+            playerChoice = oneTime;
+            return oneTime;
+        }
+        if (playerChoice == 1 && winningDoor == 2) {
+            playerChoice = 2;
+            //return 3;
+            return playerChoice;
+        }
+        if (playerChoice == 1 && winningDoor == 3) {
+            playerChoice = 3;
+            //return 2;
+            return playerChoice;
+        }
+        if (playerChoice == 2 && winningDoor == 1) {
+            playerChoice = 1;
+            //return 3;
+            return playerChoice;
+        }
+        if (playerChoice == 2 && winningDoor == 2) {
+            int twoTime = (int) (Math.random() * 2);
+            if (twoTime == 0) {
+                playerChoice = 3;
+                //return 1;
+                return playerChoice;
+            }
+            if (twoTime == 1) {
+                playerChoice = 1;
+                //return 3;
+                return playerChoice;
+            }
+        }
+        if (playerChoice == 2 && winningDoor == 3) {
+            playerChoice = 3;
+            //return 1;
+            return playerChoice;
+        }
+        if (playerChoice == 3 && winningDoor == 1) {
+            playerChoice = 1;
+            //return 2;
+            return playerChoice;
+        }
+        if (playerChoice == 3 && winningDoor == 2) {
+            playerChoice = 3;
+            //return 1;
+            return playerChoice;
+        }
+        if (playerChoice == 3 && winningDoor == 3) {
+            int threeTime = (int) ((Math.random() * 2) + 2);
+            if (threeTime == 2) {
+                playerChoice = 1;
+                //return 2;
+                return playerChoice;
+            }
+            if (threeTime == 3)
+                playerChoice = 2;
+                //return 3;
+                return playerChoice;
+        }
+        return 0;
 
-
+    }
+    public int goatDoor() {
+        if (playerChoice == 1 && winningDoor == 1) {
+            int oneTime = (int) (Math.random() * 2);
+            return oneTime;
+        }
+        if (playerChoice == 1 && winningDoor == 2) {
+            return 3;
+        }
+        if (playerChoice == 1 && winningDoor == 3) {
+            return 2;
+        }
+        if (playerChoice == 2 && winningDoor == 1) {
+            return 3;
+        }
+        if (playerChoice == 2 && winningDoor == 2) {
+            int twoTime = (int) (Math.random() * 2);
+            if (twoTime == 0) {
+                return 1;
+            }
+            if (twoTime == 1) {
+                return 3;
+            }
+        }
+        if (playerChoice == 2 && winningDoor == 3) {
+            return 1;
+        }
+        if (playerChoice == 3 && winningDoor == 1) {
+            return 2;
+        }
+        if (playerChoice == 3 && winningDoor == 2) {
+            return 1;
+        }
+        if (playerChoice == 3 && winningDoor == 3) {
+            int threeTime = (int) ((Math.random() * 2) + 2);
+            if (threeTime == 2) {
+                return 2;
+            }
+            if (threeTime == 3)
+                return 3;
+        }
+        return 38;
+    }
     public void setPlayerChoice(int playerChoice) {
         this.playerChoice = playerChoice;
     }
@@ -35,5 +140,9 @@ public class ChanceGame {
 
     public int getWinningDoor() {
         return winningDoor;
+    }
+
+    public int getPlayerChoice() {
+        return playerChoice;
     }
 }
--- src/ChanceGameRunner.java
+++ src/ChanceGameRunner.java
@@ -12,35 +12,20 @@ public class ChanceGameRunner {
         int firstGuess = s.nextInt();
         s.nextLine();
         ChanceGame lucki = new ChanceGame(firstGuess);
-        if (lucki.isWin(firstGuess)) {
-            ArrayList<Integer> pHolder = lucki.getNumbers();
-            pHolder.remove(pHolder.indexOf(firstGuess));
-            int doorOpen1 = (int) (Math.random() * 2);
-            System.out.println("One of the goats is behind door number " + pHolder.indexOf(doorOpen1) );
-            pHolder.remove(pHolder.indexOf(doorOpen1));
-            //System.out.println("Guess switched from " + firstGuess + " to " + pHolder.get(0) );
-        }else if (!lucki.isWin((firstGuess))) {
-            ArrayList<Integer> pHolder = lucki.getNumbers();
-            pHolder.remove(pHolder.indexOf(firstGuess));
-            pHolder.remove(pHolder.indexOf(lucki.getWinningDoor()));
-            System.out.println("One of the goats is behind door number " + pHolder.get(0));
-        }
-
+        System.out.println("One of the goats is behind door " + lucki.goatDoor());
         System.out.println("Do you want to switch your guess? Yes or No: ");
         String yesOrNo = s.nextLine();
-        ArrayList<Integer> pHolder1 = lucki.getNumbers();
+        int firstGuess2 = firstGuess;
         if (yesOrNo.equals("Yes")) {
-
-            int firstGuess2 = firstGuess;
-            firstGuess = pHolder1.get(0);
-            System.out.println("Guess switched from to " + firstGuess2 + " to " + pHolder1.get(0));
+            System.out.println("Guess switched from to " + firstGuess + " to " + lucki.doorSwitch());
         }
-        //System.out.println((lucki.getNumbers()).get(1));
-        System.out.print("The car is behind door " + lucki.getWinningDoor() + " ");
-        if (lucki.isWin(firstGuess)) {
-            System.out.print("You won the car!");
+        //System.out.print("The car is behind door " + lucki.getWinningDoor() + " ");
+        int test = lucki.getPlayerChoice();
+        System.out.println(test);
+        if (lucki.isWin(lucki.getPlayerChoice())) {
+            System.out.println("You won the car!");
         }else {
-            System.out.print(". You lost! (Or you could say you won a goat!");
+            System.out.println("The car is behind door " + lucki.getWinningDoor() + " You lost! (Or you could say you won a goat!");
         }
 
 
