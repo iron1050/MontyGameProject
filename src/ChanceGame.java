73f2f5ec172075a8446122829438923ffe2569c2
Chukwuma Dibia <chukwumadibia@gmail.com>
2023-11-03 22:57:40 -0400

    yes

--- src/ChanceGame.java
+++ src/ChanceGame.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class ChanceGame {
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ChanceGame {
+    int winningDoor = doorChoose();
+    int playerChoice;
+
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3));
+
+
+
+    public ChanceGame(int playerChoice) {
+        this.playerChoice = playerChoice;
+    }
+
+    public int doorChoose() {
+        return (int) (Math.random() * 3) + 1;
+    }
+
+    public boolean isWin(int guess) {
+        if (guess == winningDoor) {
+            return true;
+        }else { return false;}
+    }
+
+
+    public void setPlayerChoice(int playerChoice) {
+        this.playerChoice = playerChoice;
+    }
+
+   public ArrayList<Integer> getNumbers() {
+        return numbers;
+   }
+
+    public int getWinningDoor() {
+        return winningDoor;
+    }
 }
--- src/ChanceGameRunner.java
+++ src/ChanceGameRunner.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class ChanceGameRunner {
+
+import java.sql.PseudoColumnUsage;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ChanceGameRunner {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Welcome to the Monty Hall Game! \n");
+        System.out.print("Where do you think the car is? Enter 1, 2 or 3: ");
+        int firstGuess = s.nextInt();
+        s.nextLine();
+        ChanceGame lucki = new ChanceGame(firstGuess);
+        if (lucki.isWin(firstGuess)) {
+            ArrayList<Integer> pHolder = lucki.getNumbers();
+            pHolder.remove(pHolder.indexOf(firstGuess));
+            int doorOpen1 = (int) (Math.random() * 2);
+            System.out.println("One of the goats is behind door number " + pHolder.indexOf(doorOpen1) );
+            pHolder.remove(pHolder.indexOf(doorOpen1));
+            //System.out.println("Guess switched from " + firstGuess + " to " + pHolder.get(0) );
+        }else if (!lucki.isWin((firstGuess))) {
+            ArrayList<Integer> pHolder = lucki.getNumbers();
+            pHolder.remove(pHolder.indexOf(firstGuess));
+            pHolder.remove(pHolder.indexOf(lucki.getWinningDoor()));
+            System.out.println("One of the goats is behind door number " + pHolder.get(0));
+        }
+
+        System.out.println("Do you want to switch your guess? Yes or No: ");
+        String yesOrNo = s.nextLine();
+        ArrayList<Integer> pHolder1 = lucki.getNumbers();
+        if (yesOrNo.equals("Yes")) {
+
+            int firstGuess2 = firstGuess;
+            firstGuess = pHolder1.get(0);
+            System.out.println("Guess switched from to " + firstGuess2 + " to " + pHolder1.get(0));
+        }
+        //System.out.println((lucki.getNumbers()).get(1));
+        System.out.print("The car is behind door " + lucki.getWinningDoor() + " ");
+        if (lucki.isWin(firstGuess)) {
+            System.out.print("You won the car!");
+        }else {
+            System.out.print(". You lost! (Or you could say you won a goat!");
+        }
+
+
+    }
 }
